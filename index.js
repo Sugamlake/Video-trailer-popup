c906e51cb16d3a7f4df5286a948b664b47084abc
sugamlake <sugamlake@gmail.com>
2022-10-13 18:57:20 -0300

    photo and video template

---
+++ index.js
@@ -0,0 +1,13 @@
+const btnEl = document.querySelector('.btn');
+const closeIconEl = document.querySelector('.close-icon');
+const trailerContainerEl = document.querySelector('.trailer-container');
+
+
+btnEl.addEventListener('click', () => {
+  trailerContainerEl.classList.remove('active');
+} );
+
+closeIconEl.addEventListener('click', () => {
+  trailerContainerEl.classList.add('active');
+});
+
